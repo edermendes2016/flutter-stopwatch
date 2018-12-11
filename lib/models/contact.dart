3b058be411b7a1ad64bcebb8fff7989a17303b60
edermendes2016 <ederkbb@gmail.com>
2018-12-11 15:25:37 -0300

    mudanças/criacao crud

--- lib/models/contact.dart
+++ lib/models/contact.dart
@@ -1,5 +1,3 @@
-import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
 
 final String contactTable = "Contacts";
 final String idCollumn = "Id";
@@ -10,37 +8,6 @@ final String adressCollumn = "Adress";
 final String imgCollumn = "Imagem";
 final String dataCreationCollumn = "DataCreation";
 final String activeCollumn = "Active";
-
-class ContactModel {
-  static final ContactModel _instance = ContactModel.internal();
-
-  factory ContactModel() => _instance;
-
-  ContactModel.internal();
-
-  Database _db;
-
- Future<Database> get db async {
-    if (_db != null) {
-      return _db;
-    } else {
-      _db = await initDb();
-      return _db;
-    }
-  }
-
-  Future<Database>initDb() async {
-    final databasePath = await getDatabasesPath();
-    final path = join(databasePath, "contacts.db");
-
-    return await openDatabase(path, version: 1, onCreate: (Database db, int newerVersion) async{
-      await db.execute(
-        "CREATE TABLE $contactTable($idCollumn INTEGER PRIMARY KEY, $nameCollumn TEXT, $emailCollumn TEXT," 
-        "$phoneCollumn TEXT, $adressCollumn TEXT, $imgCollumn TEXT, $dataCreationCollumn DATETIME, $activeCollumn BOOL)"
-      );
-    });
-  }
-}
 
 class Contact {
   int id;
